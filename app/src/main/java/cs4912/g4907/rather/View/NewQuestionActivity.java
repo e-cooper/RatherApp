b57d45093297bb3b83cfc99116ef00caabf3b306
apaduvalli3 <apaduvalli3@users.noreply.github.com>
2015-04-15 19:29:54 -0400

    Added ability to add Single Choice questions, Changed adapted for question type spinner adapter on add question activity, added more info and results button to survey details activity

--- app/src/main/java/cs4912/g4907/rather/View/NewQuestionActivity.java
+++ app/src/main/java/cs4912/g4907/rather/View/NewQuestionActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -23,8 +25,11 @@ import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
+import cs4912.g4907.rather.Model.Choice;
 import cs4912.g4907.rather.Model.Question;
 import cs4912.g4907.rather.Model.QuestionType;
 import cs4912.g4907.rather.Model.Survey;
@@ -38,14 +43,18 @@ public class NewQuestionActivity extends Activity {
     private Button addNewQuestionButton;
     private Button finishNewQuestionButton;
     private Spinner questionTypeSpinner;
-    private ParseQueryAdapter<QuestionType> mainAdapter;
+    private String currentQuestionType;
+    private String surveyId;
+
+    private EditText choice1Field;
+    private EditText choice2Field;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         question = new Question();
 
         Intent i = getIntent();
-        final String surveyId = i.getStringExtra("survey_id");
+        surveyId = i.getStringExtra("survey_id");
         final Number[] questionOrder = new Number[1];
         questionOrder[0] = i.getIntExtra("order_number", 1);
 
@@ -66,44 +75,55 @@ public class NewQuestionActivity extends Activity {
             }
         });
 
-        mainAdapter = new ParseQueryAdapter<>(this, new ParseQueryAdapter.QueryFactory<QuestionType>() {
-            public ParseQuery create() {
-                ParseQuery query = new ParseQuery("QuestionType");
-                return query;
-            }
-        });
-        mainAdapter.setTextKey("name");
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_question);
 
         surveyTextView = (TextView) findViewById(R.id.new_question_survey_label);
         contentField = (EditText) findViewById(R.id.new_question_content_input);
+        choice1Field = (EditText) findViewById(R.id.new_question_choice_1);
+        choice2Field = (EditText) findViewById(R.id.new_question_choice_2);
         addNewQuestionButton = (Button) findViewById(R.id.add_question);
         finishNewQuestionButton = (Button) findViewById(R.id.finish_question);
         questionTypeSpinner = (Spinner) findViewById(R.id.new_question_spinner);
 
-        questionTypeSpinner.setAdapter(mainAdapter);
-        questionTypeSpinner.setSelection(1);
+        choice1Field.setVisibility(View.GONE);
+        choice2Field.setVisibility(View.GONE);
+
+        ParseQuery<QuestionType> query = ParseQuery.getQuery("QuestionType");
+        query.findInBackground(new FindCallback<QuestionType>() {
+            public void done(List<QuestionType> questionTypesList, ParseException e) {
+                if (e == null) {
+                    ArrayList<String> questionTypeNames = new ArrayList<String>();
+
+                    for (int i = 0; i < questionTypesList.size(); i++) {
+                        questionTypeNames.add(questionTypesList.get(i).getName());
+                    }
+
+                    //Manually order spinner elements here
+                    //Make Yes/No the first item
+                    int yesNoIndex = questionTypeNames.indexOf("Yes/No");
+                    questionTypeNames.add(0, questionTypeNames.get(yesNoIndex));
+                    int yesNoLastIndex = questionTypeNames.lastIndexOf("Yes/No");
+                    questionTypeNames.remove(yesNoLastIndex);
+                    //Make Single Choice the second item
+                    int singleChoiceIndex = questionTypeNames.indexOf("Single Choice");
+                    questionTypeNames.add(1, questionTypeNames.get(singleChoiceIndex));
+                    int singleChoiceLastIndex = questionTypeNames.lastIndexOf("Single Choice");
+                    questionTypeNames.remove(singleChoiceLastIndex);
+
+                    questionTypeSpinner.setAdapter(new ArrayAdapter<String>(NewQuestionActivity.this, android.R.layout.simple_spinner_item, questionTypeNames));
+                    currentQuestionType = questionTypeNames.get(0);
+                    setChoiceFieldsVisibility();
+                } else {
+                    Log.d("Retrieve failed", "Error: " + e.getMessage());
+                }
+            }
+        });
+
         questionTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                QuestionType selectedQuestionType = (QuestionType) questionTypeSpinner.getSelectedItem();
-                String name = selectedQuestionType.getName();
-                ParseQuery questionTypeQuery = new ParseQuery("QuestionType");
-                questionTypeQuery.whereEqualTo("name", name);
-                questionTypeQuery.getFirstInBackground(new GetCallback<ParseObject>() {
-                    public void done(ParseObject object, ParseException e) {
-                        if (object == null) {
-                            finish();
-                            Toast.makeText(
-                                    getApplicationContext(),
-                                    "Error: " + e.getMessage(),
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            question.setQuestionType(object);
-                        }
-                    }
-                });
+                currentQuestionType = questionTypeSpinner.getSelectedItem().toString();
+                setChoiceFieldsVisibility();
             }
             public void onNothingSelected(AdapterView<?> parent) {
                 String selectedQuestionType = questionTypeSpinner.getSelectedItem().toString();
@@ -114,36 +134,12 @@ public class NewQuestionActivity extends Activity {
         addNewQuestionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                question.setOrder(questionOrder[0]);
-                question.setContent(contentField.getText().toString());
-
-                question.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e == null) {
-                            setResult(RESULT_OK);
-                            Intent i = new Intent(NewQuestionActivity.this, NewQuestionActivity.class);
-                            i.putExtra("survey_id", surveyId);
-                            i.putExtra("order_number", (int) questionOrder[0] + 1);
-                            startActivity(i);
-                            finish();
-                        } else {
-                            Toast.makeText(
-                                    getApplicationContext(),
-                                    "Error saving: " + e.getMessage(),
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                saveQuestion(questionOrder[0],contentField.getText().toString(),"next");
             }
         });
 
         finishNewQuestionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                question.setOrder(questionOrder[0]);
-                question.setContent(contentField.getText().toString());
-
                 //Publish the survey
                 Survey parentSurvey;
                 try{
@@ -157,21 +153,7 @@ public class NewQuestionActivity extends Activity {
                             "Error, couldn't publish Survey: " + e.getMessage(),
                             Toast.LENGTH_SHORT).show();
                 }
-
-                question.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e == null) {
-                            setResult(RESULT_OK);
-                            finish();
-                        } else {
-                            Toast.makeText(
-                                    getApplicationContext(),
-                                    "Error saving: " + e.getMessage(),
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                saveQuestion(questionOrder[0],contentField.getText().toString(),"finish");
             }
         });
     }
@@ -180,4 +162,112 @@ public class NewQuestionActivity extends Activity {
         surveyTextView.setText(question.getSurvey().getString("title"));
     }
 
+    private void saveQuestion(final Number order, final String content, final String buttonPressed){
+        ParseQuery questionTypeQuery = new ParseQuery("QuestionType");
+        questionTypeQuery.whereEqualTo("name", currentQuestionType);
+        questionTypeQuery.getFirstInBackground(new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (object == null) {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    question.setQuestionType(object);
+                    question.setContent(content);
+                    question.setOrder(order);
+                    if(buttonPressed.equals("finish")){
+                        question.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                            if (e == null) {
+                                saveChoices(question);
+                                setResult(RESULT_OK);
+                                finish();
+                            } else {
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Error saving: " + e.getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            }
+                        });
+                    }
+                    else if(buttonPressed.equals("next")){
+                        question.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                            if (e == null) {
+                                saveChoices(question);
+                                setResult(RESULT_OK);
+                                Intent i = new Intent(NewQuestionActivity.this, NewQuestionActivity.class);
+                                i.putExtra("survey_id", surveyId);
+                                i.putExtra("order_number", (int) order + 1);
+                                startActivity(i);
+                                finish();
+                            } else {
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Error saving: " + e.getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
+
+    private void setChoiceFieldsVisibility(){
+        if(currentQuestionType.equals("Single Choice")){
+            choice1Field.setVisibility(View.VISIBLE);
+            choice2Field.setVisibility(View.VISIBLE);
+        }
+        else{
+            choice1Field.setVisibility(View.GONE);
+            choice2Field.setVisibility(View.GONE);
+        }
+    }
+
+    private void saveChoices(Question q){
+        if(currentQuestionType.equals("Single Choice")){
+            Choice choice1 = new Choice();
+            Choice choice2 = new Choice();
+
+            choice1.setOrder(1);
+            choice1.setContent(choice1Field.getText().toString());
+            choice1.setQuestion(q);
+
+            choice2.setOrder(2);
+            choice2.setContent(choice2Field.getText().toString());
+            choice2.setQuestion(q);
+
+            choice1.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                    } else {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Error saving: " + e.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            choice2.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                    } else {
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Error saving: " + e.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
 }
