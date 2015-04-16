19b7bbcb8313dd91958ca3abea12122a97d17ba2
e-cooper <elicooper18@gmail.com>
2015-04-15 21:53:25 -0400

    ability to answer single choice and boolean question types with proper swiping

--- app/src/main/java/cs4912/g4907/rather/View/ResponseActivity.java
+++ app/src/main/java/cs4912/g4907/rather/View/ResponseActivity.java
@@ -1,44 +1,61 @@
 package cs4912.g4907.rather.View;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cs4912.g4907.rather.Model.Question;
 import cs4912.g4907.rather.Model.Response;
 import cs4912.g4907.rather.R;
-import cs4912.g4907.rather.Utilities.OnSwipeTouchListener;
 
-public class ResponseActivity extends Activity {
+public class ResponseActivity extends FragmentActivity {
 
     private Response response;
     private boolean canAnswer = false;
     private boolean answered = false;
+    private String questionType;
 
     private TextView questionLabel;
-    private Button yesButton, noButton, submitButton;
-    private FrameLayout frameLayout;
+    private Button submitButton;
+    private List<Fragment> fragments;
+    private List<String> colors;
+    private ParseObject[] choices = new ParseObject[2];
+
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        colors = new ArrayList<>();
+        colors.add("#84BD00");
+        colors.add("#F44336");
+        colors.add("#000000");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_response);
 
         questionLabel = (TextView) findViewById(R.id.response_question_label);
-        yesButton = (Button) findViewById(R.id.response_button_yes);
-        noButton = (Button) findViewById(R.id.response_button_no);
         submitButton = (Button) findViewById(R.id.response_button_answer);
-        frameLayout = (FrameLayout) findViewById(R.id.fragmentContainer);
 
         response = new Response();
         Intent g = getIntent();
@@ -57,52 +74,6 @@ public class ResponseActivity extends Activity {
                     response.setResponseSet(object);
                     setResponseQuestion();
                 }
-            }
-        });
-
-        frameLayout.setOnTouchListener(new OnSwipeTouchListener(this) {
-            public void onSwipeTop() {
-                //Toast.makeText(ResponseActivity.this, "top", Toast.LENGTH_SHORT).show();
-            }
-
-            public void onSwipeRight() {
-                if (canAnswer) {
-                    response.setYesNo(true);
-                    answered = true;
-                    Toast.makeText(
-                            getApplicationContext(),
-                            R.string.response_yes_recorded,
-                            Toast.LENGTH_SHORT
-                    ).show();
-                } else {
-                    Toast.makeText(
-                            getApplicationContext(),
-                            "Answer could not be set",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                }
-            }
-
-            public void onSwipeLeft() {
-                if (canAnswer) {
-                    response.setYesNo(false);
-                    answered = true;
-                    Toast.makeText(
-                            getApplicationContext(),
-                            R.string.response_no_recorded,
-                            Toast.LENGTH_SHORT
-                    ).show();
-                } else {
-                    Toast.makeText(
-                            getApplicationContext(),
-                            "Answer could not be set",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                }
-            }
-
-            public void onSwipeBottom() {
-                //Toast.makeText(ResponseActivity.this, "bottom", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -150,11 +121,108 @@ public class ResponseActivity extends Activity {
                                         Toast.LENGTH_SHORT).show();
                             } else {
                                 response.setQuestion(object);
-                                canAnswer = true;
                                 questionLabel.setText(object.getString("content"));
+                                setQuestionOptions(object);
                             }
                         }
                     });
+                }
+            }
+        });
+    }
+
+    private void setQuestionOptions(ParseObject question) {
+        final ParseObject q = question;
+        ParseObject qt = question.getParseObject("questionType");
+        qt.fetchInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject parseObject, ParseException e) {
+                if (e == null) {
+                    questionType = parseObject.getString("name");
+                    if (questionType.equals("Yes/No")) {
+                        fragments = new ArrayList<>();
+                        fragments.add(ResponseFragment.newInstance("Yes", colors.get(0)));
+                        fragments.add(ResponseFragment.newInstance("Swipe to choose", colors.get(2)));
+                        fragments.add(ResponseFragment.newInstance("No", colors.get(1)));
+
+                        mPager = (ViewPager) findViewById(R.id.pager);
+                        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+                        mPager.setAdapter(mPagerAdapter);
+                        mPager.setCurrentItem(1);
+                        canAnswer = true;
+                        setAdapterListener();
+                    } else if (questionType.equals("Single Choice")) {
+                        fragments = new ArrayList<>();
+                        ParseQuery choicesQuery = new ParseQuery("Choice");
+                        choicesQuery.whereEqualTo("question", q);
+                        choicesQuery.findInBackground(new FindCallback<ParseObject>() {
+                            @Override
+                            public void done(List<ParseObject> list, ParseException e) {
+                                if (e == null) {
+                                    for (int i = 0; i < list.size(); i++) {
+                                        ParseObject o = list.get(i);
+                                        choices[i] = o;
+                                        String content = o.getString("content");
+                                        fragments.add(ResponseFragment.newInstance(content, colors.get(i)));
+                                    }
+                                    fragments.add(1, ResponseFragment.newInstance("Swipe to choose", colors.get(2)));
+
+                                    mPager = (ViewPager) findViewById(R.id.pager);
+                                    mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+                                    mPager.setAdapter(mPagerAdapter);
+                                    mPager.setCurrentItem(1);
+                                    canAnswer = true;
+                                    setAdapterListener();
+                                } else {
+                                    finish();
+                                    Toast.makeText(
+                                            getApplicationContext(),
+                                            "Error: " + e.getMessage(),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
+    }
+
+    public void setAdapterListener() {
+        mPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (canAnswer) {
+                    if (questionType.equals("Single Choice")) {
+                        response.setChoice(choices[position]);
+                    } else if (questionType.equals("Yes/No")) {
+                        if (position == 0) {
+                            response.setYesNo(true);
+                        } else {
+                            response.setYesNo(false);
+                        }
+                    }
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (state == ViewPager.SCROLL_STATE_IDLE && !answered) {
+                    int cPage = mPager.getCurrentItem();
+                    fragments.remove(1);
+                    answered = true;
+                    mPagerAdapter.notifyDataSetChanged();
+                    mPager.setAdapter(mPagerAdapter);
+                    if (cPage != 0) {
+                        mPager.setCurrentItem(1);
+                    }
                 }
             }
         });
@@ -209,6 +277,22 @@ public class ResponseActivity extends Activity {
                 }
             }
         });
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 
 }
